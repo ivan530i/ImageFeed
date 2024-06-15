38cac36a29969d7e6d9746dcfcf3a5e3c07fe8db
ivan530i <pchelnikov79@gmail.com>
2024-06-15 20:52:25 +0300

    refactoring ImagesListViewController

---
+++ ImagesList/ImagesListPresenter.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+final class ImagesListPresenter: ImagesListPresenterProtocol {
+    weak var view: ImagesListViewProtocol?
+    private let imageListService: ImagesListService
+    
+    private(set) var photos: [Photo] = []
+    
+    init(view: ImagesListViewProtocol, service: ImagesListService = .shared) {
+        self.view = view
+        self.imageListService = service
+        NotificationCenter.default.addObserver(self, selector: #selector(updatePhotos), name: ImagesListService.didChangeNotification, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    func viewDidLoad() {
+        imageListService.fetchPhotosNextPage()
+    }
+    
+    func didTapLikeButton(at indexPath: IndexPath) {
+        let photo = photos[indexPath.row]
+        UIBlockingProgressHUD.show()
+        
+        imageListService.changeLike(photoId: photo.id, isLike: !photo.isLiked) { [weak self] result in
+            guard let self = self else { return }
+            
+            DispatchQueue.main.async {
+                UIBlockingProgressHUD.dismiss()
+                
+                switch result {
+                case .success:
+                    self.photos = self.imageListService.photos
+                    self.view?.updateTableView()
+                case .failure(let error):
+                    self.view?.showError(message: error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    @objc private func updatePhotos() {
+        photos = imageListService.photos
+        view?.updateTableView()
+    }
+}
